a5a3e57747471349c6ce9af2fbf808e829e12cb1
1195655796 <1195655796@qq.com>
2024-05-20 17:50:27 +1000

    refactor: make matrix multiply to be concurrency with threads

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 mod matrix;
+mod vector;
+pub use matrix::{multiply, Matrix};
+pub use vector::{Vector, dot_product};
 
-pub use matrix::{Matrix, multiply};
---
+++ src/vector.rs
@@ -0,0 +1,34 @@
+use anyhow::{Result, anyhow};
+use std::ops::{Add, AddAssign, Deref, Mul};
+pub struct Vector<T> {
+    data: Vec<T>,
+}
+
+impl<T> Deref for Vector<T> {
+    type Target = Vec<T>;
+    fn deref(&self) -> &Self::Target {
+        &self.data
+    }
+}
+
+impl<T> Vector<T> {
+    pub fn new(data: impl Into<Vec<T>>) -> Self {
+        Self { data: data.into() }
+    }
+}
+
+// pretend this is a heavy operation, CPU intensive
+pub fn dot_product<T>(a: Vector<T>, b: Vector<T>) -> Result<T>
+where
+    T: Add<Output = T> + Mul<Output = T> + AddAssign + Default + Copy,
+{
+    if a.len() != b.len() {
+        // a.len() => a.data.len() (Deref trait)
+        return Err(anyhow!("Dot product error: a.len != b.len"));
+    }
+    let mut sum = T::default();
+    for i in 0..a.len() {
+        sum += a[i] * b[i];
+    }
+    Ok(sum)
+}
